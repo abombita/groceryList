30d64153906b8af5eb5057b1a86ede3f5a040581
Astraea Bombita <abombita98@hotmail.com>
2021-07-19 15:58:51 -0400

    got to connect to data base and show on site

--- index.js
+++ index.js
@@ -8,26 +8,31 @@ app.set('view engine','ejs');
 
 const connection = mysql.createConnection({
     host     : 'localhost',
-    port : 8888, 
     user     : 'root',
-    password : '',
-    database: 'groceries'
+    password : 'root',
+    database: 'groceries',
+    port : 8889
 });
+connection.connect;
 
-connection.connect();
+app.get('/', function(req, res){
 
-app.get('/', function(req,res){
-
-    connection.query('SELECT id,item,quantity FROM grocerylist', function(err,results){
+    connection.query('SELECT id, item, quantity FROM grocerylist', function(err,results){
+        console.log('current results')
         console.log(results);
-        
+
         res.render('index', {
             groceryList: results
-        });
+        }); 
+    });
+});
+
+app.post('/', function(req,res){
+    let grocery = req.body.grocery;
+    connection.query('INSERT INTO grocerylist (item) VALUES (?)', grocery, function(err,results){
+        res.redirect('/');
     });
 });
 
 
-
-
-app.listen(8000);
+app.listen(3000);
